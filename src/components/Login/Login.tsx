2bf30baac4f4c6cbe0e4e115adb42036fadc0b5e
Filip Petrovski <filip.petrovski@webfactory.mk>
2022-10-25 14:55:50 +0200

    Add redux, redux-toolkit and bootstrap packages to project

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,10 +4,11 @@ import classes from '../Login/Login.module.scss';
 const Login = () => {
 	const signInHandler = () => {
 		console.log('sign in clicked');
-	}
+		window.window.location.href = `https://api.imgur.com/oauth2/authorize?client_id=${process.env.REACT_APP_IMGUR_CLIENT_ID}&response_type=token`;
+	};
 
 	return (<div className={classes.LoginWrapper}>
-		<img src={imgurImg} alt="Imgur logo"/>
+		<img className='img-fluid' src={imgurImg} alt="Imgur logo"/>
 		<button className={classes.Button} onClick={signInHandler}>SIGN IN</button>
 		<h1>IMGUR WEB APP</h1>
 	</div>);
